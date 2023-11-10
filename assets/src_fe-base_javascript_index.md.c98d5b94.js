e958faa529e38c52441d570df1c53e497d284b9f
chuangzhen <394405852@qq.com>
2023-11-10 16:18:01 +0800

    feat: 初始化搭建基于vitepress 的个人博客

---
+++ assets/src_fe-base_javascript_index.md.c98d5b94.js
@@ -0,0 +1,13 @@
+import{_ as s,o as a,c as n,Q as e}from"./chunks/framework.1484e472.js";const l="/card.png",g=JSON.parse('{"title":"这是js","description":"","frontmatter":{},"headers":[],"relativePath":"src/fe-base/javascript/index.md","filePath":"src/fe-base/javascript/index.md","lastUpdated":1697707928000}'),p={name:"src/fe-base/javascript/index.md"},o=e('<h1 id="这是js" tabindex="-1">这是js <a class="header-anchor" href="#这是js" aria-label="Permalink to &quot;这是js&quot;">​</a></h1><p><img src="'+l+`" alt="image"></p><div class="language-js vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark has-highlighted-lines vp-code-dark"><code><span class="line"><span style="color:#F97583;">export</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">default</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#B392F0;">data</span><span style="color:#E1E4E8;"> () {</span></span>
+<span class="line highlighted"><span style="color:#E1E4E8;">    </span><span style="color:#F97583;">return</span><span style="color:#E1E4E8;"> {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      msg: </span><span style="color:#9ECBFF;">&#39;Highlighted!&#39;</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"><span style="color:#E1E4E8;">}</span></span></code></pre><pre class="shiki github-light has-highlighted-lines vp-code-light"><code><span class="line"><span style="color:#D73A49;">export</span><span style="color:#24292E;"> </span><span style="color:#D73A49;">default</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">  </span><span style="color:#6F42C1;">data</span><span style="color:#24292E;"> () {</span></span>
+<span class="line highlighted"><span style="color:#24292E;">    </span><span style="color:#D73A49;">return</span><span style="color:#24292E;"> {</span></span>
+<span class="line"><span style="color:#24292E;">      msg: </span><span style="color:#032F62;">&#39;Highlighted!&#39;</span></span>
+<span class="line"><span style="color:#24292E;">    }</span></span>
+<span class="line"><span style="color:#24292E;">  }</span></span>
+<span class="line"><span style="color:#24292E;">}</span></span></code></pre></div>`,3),t=[o];function c(r,i,d,E,h,y){return a(),n("div",null,t)}const m=s(p,[["render",c]]);export{g as __pageData,m as default};
